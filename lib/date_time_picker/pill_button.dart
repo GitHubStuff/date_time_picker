ffc44c0bec07c703bc55ebfa2910a07f0b2cfda6
Steven Smith <github@icodeforyou.com>
2023-08-29 16:58:26 -0400

    Far aqua button

--- lib/date_time_picker/pill_button.dart
+++ lib/date_time_picker/pill_button.dart
@@ -1,35 +1,63 @@
 import 'package:flutter/material.dart';
 
 class AquaButton extends StatelessWidget {
-  const AquaButton({super.key});
+  const AquaButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double mainRadius = 15;
+    double northPoleRadius = mainRadius * (1.0 / 3.0);
+    double southPoleRadius = mainRadius *
+        0.5; // Slightly larger than the north pole for more dispersion
+
     return Container(
-      width: 15,
-      height: 15,
+      width: mainRadius * 2,
+      height: mainRadius * 2,
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: RadialGradient(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
           colors: [
-            Colors.red.shade500,
-            Colors.red.shade800,
+            Colors.red[900]!,
+            Colors.red,
           ],
-          stops: const [0.5, 1.0],
-          center: const Alignment(0.0, 0.0), // Centered gradient
-          radius: 0.8,
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.red.shade900,
-            blurRadius: 2.0,
-            offset: const Offset(1.0, 1.5),
+        shape: BoxShape.circle,
+      ),
+      child: Stack(
+        children: [
+          // North pole
+          Positioned(
+            left: mainRadius - northPoleRadius,
+            top: 0,
+            child: Container(
+              width: northPoleRadius * 2,
+              height: northPoleRadius * 2,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [Colors.white.withOpacity(0.7), Colors.transparent],
+                  stops: const [0.4, 1.0], // Quick fade
+                ),
+                shape: BoxShape.circle,
+              ),
+            ),
           ),
-          BoxShadow(
-            color: Colors.white.withOpacity(0.5),
-            blurRadius: 1.0,
-            offset: const Offset(-1.0, -1.0),
-            spreadRadius: 0.5,
+          // South pole
+          Positioned(
+            left: mainRadius - southPoleRadius,
+            bottom:
+                -southPoleRadius, // Shift it so that the center rests against the edge
+            child: Container(
+              width: southPoleRadius * 2,
+              height: southPoleRadius * 2,
+              decoration: BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [Colors.white.withOpacity(0.2), Colors.transparent],
+                  stops: const [0.2, 1.0], // More diffuse fade
+                ),
+                shape: BoxShape.circle,
+              ),
+            ),
           ),
         ],
       ),
